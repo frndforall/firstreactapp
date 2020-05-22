b36d1417b9664480032fe58e712527ca26eda19a
Nagendra Sharma <nagendra.sharma@cybersoft.net>
2020-05-22 11:47:35 +0530

    added 2 components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './FirstComponent';
+import SecondComponent from './SecondComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {  
+  render() {     
+     return (  
+         <div>  
+             Default Props Example <button>Click Me</button>
+           <h3>Welcome to {this.props.name}</h3>   
+           <p className="text-style">Javatpoint is one of the best Java training institute in Noida, Delhi, Gurugram, Ghaziabad and Faridabad.</p>          
+        
+         <FirstComponent/>
+         <SecondComponent/>
+         </div>  
+       );  
+   }  
+}  
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name = "My Tutorial!!" />
   </React.StrictMode>,
   document.getElementById('root')
 );
